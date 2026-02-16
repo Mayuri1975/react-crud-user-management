7d824210abed936af50121ccf35b0f54345c67ac
Mayuri Sure <141281357+Mayuri1975@users.noreply.github.com>
2026-02-16 15:06:43 +0530

    Updated API to MockAPI for deployment

--- src/api/userService.js
+++ src/api/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/users";
+const API_URL = "https://6992e4728f29113acd3f42a1.mockapi.io/users";
 
 export const getUsers = () => axios.get(API_URL);
 export const createUser = (data) => axios.post(API_URL, data);
